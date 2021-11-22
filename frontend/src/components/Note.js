a7ac2c3fbf3cf5890bd995c0a54b1bb0217325bf
Veronika Rogalevich <comedi.nika@mail.ru>
2021-11-22 23:56:59 +0300

    react components

---
+++ frontend/src/components/Note.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Rating from "./Rating";
+
+export default function Note(props){
+    const {note} = props;
+    return (
+        <div key={note.id} className="card">
+            <a href={`/note/${note.id}`}>
+                <img className="medium" src={note.image} alt={note.title}/>
+            </a>
+            <div className="card-body">
+                <a href={`/note/${note.id}`}>
+                    <h2>{note.title}</h2>
+                </a>
+                <p>{note.description}</p>
+                <p>Count of members: {note.countOfMembers}</p>
+                <p>Cost: {note.money}</p>
+                <p>Age from {note.ageFrom} to {note.ageTo}</p>
+                <Rating
+                    rating={note.rating}
+                    numReviews={note.numReviews}>
+                </Rating>
+            </div>
+        </div>
+    )
+}

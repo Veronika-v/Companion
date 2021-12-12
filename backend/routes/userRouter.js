29a47726cd279bac55e4f4269f32da96999126a5
Veronika Rogalevich <comedi.nika@mail.ru>
2021-12-12 23:04:46 +0300

    authorization token for users api

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const {isAuth} = require("../utils");
 
 
 const router = express.Router();
 router.post('/register', userController.addUser);
-router.put('/updateNote', userController.updateUser);
-router.put('/updateNote', userController.updateUserByAdmin);
-router.get('/:id', userController.findById);
-router.get('/', userController.findAll);
+router.put('/update', isAuth, userController.updateUser);
+router.put('/updateByAdmin', userController.updateUserByAdmin); //isAdmin!!!!!
+router.get('/:id', userController.findById); //isAdmin & isAuth- когда смотришь кто откликнулся!!!!!!
+router.get('/', userController.findAll); //isAdmin!!!!!
 router.post('/signIn', userController.signIn);
 
 module.exports ={
